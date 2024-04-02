4b62adfa296db3f0881b78a44ce897908b211abd
Valt1-0 <lai2val1@gmail.com>
2024-04-02 21:53:48 +0200

    Ajouter l'instrumentation pour le service de tickets

--- Tickets/src/instrumentation.js
+++ Tickets/src/instrumentation.js
@@ -1,2 +1,3 @@
 const { installInstrumentation } = require('@Telemetry/telemetry');
+console.log('Instrumentation for tickets service is being installed');
 installInstrumentation({ serviceName: 'tickets' });
